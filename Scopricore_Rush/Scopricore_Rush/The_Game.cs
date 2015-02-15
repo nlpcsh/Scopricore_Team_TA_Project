916108d599bbd5eba6206e80cd18b73ecce706dd
Nikolay Petrov <nikolay.petrov@abv.bg>
2015-02-15 14:10:04 +0200

    Initial project commit

---
+++ Scopricore_Rush/Scopricore_Rush/The_Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scopricore_Rush
+{
+    class The_Game
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
