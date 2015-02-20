00b35d4cb409c35558e99fd5e0304d7f357b3d52
kskondov <kskondov@gmail.com>
2015-02-20 12:32:39 +0200

    Merge Errors
    Something broke during the merge

--- ScorpicoreRush/ScorpicoreRush/ScorpicoreRush.cs
+++ ScorpicoreRush/ScorpicoreRush/ScorpicoreRush.cs
@@ -1,24 +1,21 @@
-﻿using System;
-using System.Threading;
-
-class ScorpicoreRush
+﻿namespace ScorpicoreRush
 {
-    static void Main()
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+
+    struct Position
     {
-        // Title of the game
-        Console.Title = "Falling Rocks Game!";
-
-        // Set game window size
-        const int WIDTH = 80;
-        const int HEIGHT = 25;
-
-        // Check the size
-        if (WIDTH <= Console.LargestWindowWidth)
+        public int X, Y;
+        public Position(int x, int y)
         {
-            Console.WindowWidth = WIDTH;
+            this.X = x;
+            this.Y = y;
         }
+    }
 
-<<<<<<< HEAD
     class ScorpicoreRush
     {
 
@@ -31,55 +28,56 @@ class ScorpicoreRush
             Random rand = new Random();
             Queue<Position> HeroPosition = new Queue<Position>();
             Position HeroStartPosition = new Position(20, 20);
-=======
-        if (HEIGHT + 1 <= Console.LargestWindowHeight)
-        {
-            Console.WindowHeight = HEIGHT + 1;
-            // For testing
-            //            Console.WindowHeight = HEIGHT + 2;
-        }
 
-        // Check the initialization
-        /*
-                Console.WriteLine(Console.WindowWidth);
-                Console.WriteLine(Console.WindowHeight);
->>>>>>> origin/master
+            HeroPosition.Enqueue(HeroStartPosition);
 
-                for (int i = 0; i < HEIGHT; i++)
-                {
-                    Console.WriteLine(i);
-                }
+            string hero = "(о)(о)";
 
-                Console.ReadLine();
-        */
-        char[] rockSymbols = { '^', '@', '*', '&', '+', '%', '$', '#', '!', '.', ';' }; // 11 elements. Rocks are the symbols ^, @, *, &, +, %, $, #, !, ., ;, - distributed with appropriate density.
+            Console.CursorVisible = false;
+            Console.BufferHeight = Console.WindowHeight;
+            Console.BufferWidth = Console.WindowWidth;
 
-        int dwarfPosition = (WIDTH - 1) / 2;
+            //moving the Hero
 
-        int difficulty = 30; // % of each row covered with rocks
-        int rocksPerRow = WIDTH * difficulty / 100; // The max number of rocks per row. Integer division
+            Console.ForegroundColor = ConsoleColor.DarkYellow;      //<---------Eventually change color with levels
+            Console.SetCursorPosition(20, 20);
+            Console.WriteLine(hero);
 
-        // Initializing the result
-        ulong score = 0;
+            //TODO: Implement movements --  Move Up and Down 
 
-        // The array of rocks - declaration and initialization
-        char[,] rocks = new char[HEIGHT + 1, WIDTH + 1]; // The first element of each row keeps the number of rocks contained - for easier calculation of the score
-
-        // Initialization
-        for (int i = 0; i <= HEIGHT; i++)
-        {
-            rocks[i, 0] = (char)0;
-            for (int j = 1; j <= WIDTH; j++)
+            while (true)
             {
-                rocks[i, j] = ' ';
-            }
-        }
+                Console.CursorVisible = false;
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    if (key.Key == ConsoleKey.Spacebar)
+                    {
+                        foreach (Position position in HeroPosition)
+                        {
+                            Console.SetCursorPosition(position.X, position.Y);
+                            Console.WriteLine(hero);
+                        }
+                    }
 
-        int waitTime = 150;
+                    else if (key.Key == ConsoleKey.LeftArrow)
+                    {
 
-        bool success = true;
+                        HeroPosition.Dequeue();
+                        Position dwarfEnd = new Position(HeroStartPosition.X -= 1, HeroStartPosition.Y);
+                        HeroPosition.Enqueue(dwarfEnd);
+                        Console.Clear();
+                        foreach (Position position in HeroPosition)
+                        {
+                            Console.SetCursorPosition(position.X - 1, position.Y);
+                            Console.WriteLine(hero);
+                            if (HeroStartPosition.X == 1)
+                            {
+                                HeroStartPosition.X = HeroStartPosition.X + 1;
+                            }
+                        }
+                    }
 
-<<<<<<< HEAD
                     else if (key.Key == ConsoleKey.RightArrow)
                     {
                         HeroPosition.Dequeue();
@@ -107,137 +105,22 @@ class ScorpicoreRush
                         Console.ResetColor();
                         Menu.ShowMenu();
                     }
-=======
-        while (success)
-        {
-            // Clear buffer area
-            while (Console.KeyAvailable)
-            {
-                ConsoleKeyInfo key = Console.ReadKey();
-                if (key.Key == ConsoleKey.LeftArrow && dwarfPosition > 1)
-                {
-                    dwarfPosition--;
                 }
-                else if (key.Key == ConsoleKey.RightArrow && dwarfPosition < WIDTH)
-                {
-                    dwarfPosition++;
-                }
-                else // Do nothing
-                {
-                    ;
->>>>>>> origin/master
-                }
-            }
 
-<<<<<<< HEAD
                 //Thread.Sleep(20);
                 
 
-=======
-            // Move all rows downwards (Falling)
-            for (int i = 0; i < HEIGHT; i++)
-            {
-                for (int j = 0; j <= WIDTH; j++)
-                {
-                    rocks[i, j] = rocks[i + 1, j];
-                }
+
             }
 
-            // Generate a new row on the top
->>>>>>> origin/master
-
-            // Initialize
-            for (int j = 0; j <= WIDTH; j++)
-            {
-                rocks[HEIGHT, j] = ' ';
-            }
-
-            Random random = new Random();
-            int randomRocksPerRow = random.Next(0, rocksPerRow); // Random number of the rocks per row between 0 and rocksPerRow (rocksPerRow not included)
-            //            Console.WriteLine(randomRocksPerRow);
-            //            Console.ReadLine();
-
-            rocks[HEIGHT, 0] = (char)randomRocksPerRow;
-            for (int i = 0; i < randomRocksPerRow; i++)
-            {
-                int nextPosition = random.Next(1, WIDTH + 1); // A random position between 1 and WIDTH (inclusive)
-                int nextRockType = random.Next(0, rockSymbols.Length); // A random type of rock
-                rocks[HEIGHT, nextPosition] = rockSymbols[nextRockType];
-            }
-
-            // Check the bottom row for a hit on the dwarf
-            if (rocks[0, dwarfPosition] == ' ' && rocks[0, dwarfPosition + 1] == ' ' && rocks[0, dwarfPosition + 2] == ' ') // No hit
-            {
-                // Success. We have successfully evaded the rocks and we turn them into "!"'s
-                for (int i = 1; i <= WIDTH; i++)
-                {
-                    if (rocks[0, i] != ' ')
-                    {
-                        rocks[0, i] = '!';
-                    }
-                }
-
-                rocks[0, dwarfPosition] = '(';
-                rocks[0, dwarfPosition + 1] = '0';
-                rocks[0, dwarfPosition + 2] = ')';
-
-                score += (ulong)rocks[0, 0];
-            }
-            else
-            {
-                success = false;
-                if (rocks[0, dwarfPosition] != ' ')
-                {
-                    rocks[0, dwarfPosition] = 'X';
-                }
-                else
-                {
-                    rocks[0, dwarfPosition] = '(';
-                }
-
-                if (rocks[0, dwarfPosition + 1] != ' ')
-                {
-                    rocks[0, dwarfPosition + 1] = 'X';
-                }
-                else
-                {
-                    rocks[0, dwarfPosition + 1] = '0';
-                }
-
-                if (rocks[0, dwarfPosition + 2] != ' ')
-                {
-                    rocks[0, dwarfPosition + 2] = 'X';
-                }
-                else
-                {
-                    rocks[0, dwarfPosition + 2] = ')';
-                }
-            }
-
-            // Print the new status
-            Console.Clear();
-
-            for (int i = HEIGHT; i >= 0; i--)
-            {
-                for (int j = 1; j <= WIDTH; j++)
-                {
-                    Console.Write(rocks[i, j]);
-                }
-                if (i > 0)
-                {
-                    //                   Console.WriteLine();
-                }
-            }
-
-            // Console.ReadLine();
-            Thread.Sleep(waitTime);
-        }
-
-        Console.Write("You scored: {0}! ", score);
-        // Clear buffer area to observe score
-        while (Console.KeyAvailable)
-        {
-            ConsoleKeyInfo key = Console.ReadKey();
         }
     }
 }
+
+
+
+
+
+
+
+
