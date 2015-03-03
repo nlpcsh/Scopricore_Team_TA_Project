abab5c4e13db8ecb5ccdb8e866facebe96ee743c
elfoles <stefan.mirevski@gmail.com>
2015-03-03 18:34:31 +0200

    Factoring out actions into functions in Game.cs

--- ScorpicoreRush/ScorpicoreRush/Game.cs
+++ ScorpicoreRush/ScorpicoreRush/Game.cs
@@ -10,61 +10,72 @@ namespace ScorpicoreRush
 {
     class Game
     {
-        static int windowWidth = 100;
-        static int windowHeight = 30;
+        static int WindowWidth = 100;
+        static int WindowHeight = 30;
 
-        static int gameFieldWidth = 70;
-        static int gameFieldHeight = 20;
+        static int GameFieldWidth = 70;
+        static int GameFieldHeight = 20;
 
-        static int difficulty = 10; // % of each row covered with rocks
-        static int timeToSleep = 200;
-        static int maxRocksPerRow = 10; // The max number of rocks per row. Integer division
-
-        static char[] rockSymbols = { '$', '&', '#' }; // shoot $ avoid & and #, # is indestructable...  
-        static char playerLifeSymbol = '\u2665'; // A heart
-
-        static char[,] rocks = new char[gameFieldHeight + 1, gameFieldWidth];    // The first row keeps new rocks positions 
+        static char[] RockSymbols = { '$', '&', '#' }; // shoot $ avoid & and #, # is indestructable...  
+        static char PlayerLifeSymbol = '\u2665'; // A heart
 
         // To ensure the randomness :) 
         static Random Rand = new Random();
         static Random RockRandom = new Random();
 
+        static int TimeToSleep = 200;
+
+        static int difficulty = 10; // % of each row covered with rocks
+        static int maxRocksPerRow = 10; // The max number of rocks per row. Integer division
+
+        static char[,] rocksMatrix = new char[GameFieldHeight + 1, GameFieldWidth]; // The first row keeps new rocks positions
+
         static bool isPlaying = true;
 
         static Scorpicore hero = new Scorpicore();
 
-        public static void Play()
+        public static void SetUpWindow()
         {
             Console.Title = "Scorpicore Rush!";
             Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.CursorVisible = false;
-            Console.BufferHeight = Console.WindowHeight = windowHeight;
-            Console.BufferWidth = Console.WindowWidth = windowWidth;
+            Console.BufferHeight = Console.WindowHeight = WindowHeight;
+            Console.BufferWidth = Console.WindowWidth = WindowWidth;
+        }
 
-            //Menu.ShowMenu();
-            Console.Clear();
-
-            hero.x = gameFieldWidth / 2;
-            hero.y = gameFieldHeight;
+        public static void InitializeHero()
+        {
+            hero.x = GameFieldWidth / 2;
+            hero.y = GameFieldHeight;
             hero.avatar = "<^>";
             hero.level = 1;
             hero.lives = 3;
             hero.points = 0;
             hero.color = ConsoleColor.Blue;
+        }
+
+        public static void Play()
+        {
+            SetUpWindow();
+            //Menu.ShowMenu();
+            Console.Clear();
+
+            InitializeHero();
 
             string bullet = "o";
             string currentWeapon = "W";
 
-            // initial rock matrix initialization
-            InitialRocksInitialization();
+            InitializeRocksMatrix();
 
             while (true)
             {
-
                 if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey(true);
-                    while (Console.KeyAvailable) Console.ReadKey(true); // clears the ReadKey buffer
+                    while (Console.KeyAvailable)
+                    {
+                        Console.ReadKey(true); // clears the ReadKey buffer
+                    }
 
                     HeroMovementAndGameControls(key);
 
@@ -92,7 +103,7 @@ namespace ScorpicoreRush
                             }
 
                             Console.Clear();
-                            PrintMenu(hero.points, playerLifeSymbol, hero.level);
+                            PrintMenu(hero.points, PlayerLifeSymbol, hero.level);
                             DrawGameMenu();
                             DrawDownBorder();
                             GenerateNewRowRocks();
@@ -103,15 +114,15 @@ namespace ScorpicoreRush
                             DoesHeroHitARock();
 
                             // bullet hit a rock !  // TODO - need to be improved
-                            if ((rocks[y, x] != ' ') || (rocks[y + 1, x] != ' '))
+                            if ((rocksMatrix[y, x] != ' ') || (rocksMatrix[y + 1, x] != ' '))
                             {
                                 using (SoundPlayer bulletHitARockSound = new SoundPlayer(@"..\..\Pop_And_Explosion.wav"))
                                 {
                                     bulletHitARockSound.Play();
                                 }
-                                rocks[y, x] = ' ';
-                                rocks[y + 1, x] = ' '; // if the bullet jump over the rock ( from y-=2 )
-                                rocks[y - 1, x] = ' ';
+                                rocksMatrix[y, x] = ' ';
+                                rocksMatrix[y + 1, x] = ' '; // if the bullet jump over the rock ( from y-=2 )
+                                rocksMatrix[y - 1, x] = ' ';
                                 hero.points++;
                                 break;
                             }
@@ -139,7 +150,7 @@ namespace ScorpicoreRush
                                 y -= 2; // faster movement of the bullet
                             }
 
-                            Thread.Sleep(timeToSleep);
+                            Thread.Sleep(TimeToSleep);
                         }
                         //});
                         //currentWeapon = SelectWeapon();
@@ -149,7 +160,7 @@ namespace ScorpicoreRush
                 }
 
                 Console.Clear();
-                PrintMenu(hero.points, playerLifeSymbol, hero.level);
+                PrintMenu(hero.points, PlayerLifeSymbol, hero.level);
                 DrawGameMenu();
                 DrawDownBorder();
                 GenerateNewRowRocks();
@@ -159,13 +170,13 @@ namespace ScorpicoreRush
                 // Hero hit a rock !
                 DoesHeroHitARock();
                 
-                Thread.Sleep(timeToSleep);
+                Thread.Sleep(TimeToSleep);
             }
         }
 
         public static void DoesHeroHitARock()
         {
-            if ((rocks[hero.y, hero.x] != ' ') || (rocks[hero.y, hero.x + 1] != ' ') || (rocks[hero.y, hero.x + 2] != ' '))
+            if ((rocksMatrix[hero.y, hero.x] != ' ') || (rocksMatrix[hero.y, hero.x + 1] != ' ') || (rocksMatrix[hero.y, hero.x + 2] != ' '))
             {
 
                 hero.lives -= 1;
@@ -190,14 +201,14 @@ namespace ScorpicoreRush
             Menu.SelectOptions();
         }
 
-        public static void InitialRocksInitialization()
+        public static void InitializeRocksMatrix()
         {
             // initial rock matrix initialization
-            for (int row = 0; row < gameFieldHeight; row++)
+            for (int row = 0; row < GameFieldHeight; row++)
             {
-                for (int col = 0; col < gameFieldWidth; col++)
+                for (int col = 0; col < GameFieldWidth; col++)
                 {
-                    rocks[row, col] = ' ';
+                    rocksMatrix[row, col] = ' ';
                 }
             }
         }
@@ -210,14 +221,14 @@ namespace ScorpicoreRush
             }
             isPlaying = false; // ensure breaking the bullets while cycle
             Console.Clear();
-            PrintMenu(hero.points, playerLifeSymbol, hero.level);
+            PrintMenu(hero.points, PlayerLifeSymbol, hero.level);
             DrawGameMenu();
             DrawDownBorder();
 
-            InitialRocksInitialization();
+            InitializeRocksMatrix();
 
-            hero.x = gameFieldWidth / 2;
-            hero.y = gameFieldHeight;
+            hero.x = GameFieldWidth / 2;
+            hero.y = GameFieldHeight;
         }
 
         public static void HeroMovementAndGameControls(ConsoleKeyInfo key)
@@ -231,7 +242,7 @@ namespace ScorpicoreRush
             }
             else if (key.Key == ConsoleKey.RightArrow)
             {
-                if (hero.x + 1 < gameFieldWidth)
+                if (hero.x + 1 < GameFieldWidth)
                 {
                     hero.x += 1;
                 }
@@ -245,7 +256,7 @@ namespace ScorpicoreRush
             }
             else if (key.Key == ConsoleKey.DownArrow)
             {
-                if (hero.y + 1 <= gameFieldHeight)
+                if (hero.y + 1 <= GameFieldHeight)
                 {
                     hero.y += 1;
                 }
@@ -272,28 +283,28 @@ namespace ScorpicoreRush
 
         public static void PrintMenu(int points, char playerLives, int level)
         {
-            PrintTextInGameMenu(gameFieldWidth + 22, 4, points.ToString(), ConsoleColor.Gray);
-            PrintTextInGameMenu(gameFieldWidth + 20, 6, new string(playerLives, hero.lives), ConsoleColor.Red);  //TODO: Print this according to the logic for loosing lives 
-            PrintTextInGameMenu(gameFieldWidth + 22, 8, level.ToString(), ConsoleColor.Gray);
+            PrintTextInGameMenu(GameFieldWidth + 22, 4, points.ToString(), ConsoleColor.Gray);
+            PrintTextInGameMenu(GameFieldWidth + 20, 6, new string(playerLives, hero.lives), ConsoleColor.Red);  //TODO: Print this according to the logic for loosing lives 
+            PrintTextInGameMenu(GameFieldWidth + 22, 8, level.ToString(), ConsoleColor.Gray);
         }
 
         public static void DrawGameMenu()
         {
-            for (int i = 0; i < gameFieldHeight + 1; i++)
+            for (int i = 0; i < GameFieldHeight + 1; i++)
             {
-                PrintOnPosition(gameFieldWidth + 2, i, "||", ConsoleColor.DarkGray);
+                PrintOnPosition(GameFieldWidth + 2, i, "||", ConsoleColor.DarkGray);
             }
-            PrintTextInGameMenu(gameFieldWidth + 4, 0, "--------------------------", ConsoleColor.DarkGray);
-            PrintTextInGameMenu(gameFieldWidth + 6, 1, "*** SCORPICORE RUSH ***", ConsoleColor.DarkGray);
-            PrintTextInGameMenu(gameFieldWidth + 4, 2, "--------------------------", ConsoleColor.DarkGray);
-            PrintTextInGameMenu(gameFieldWidth + 11, 4, "Points: ", ConsoleColor.DarkGray);
-            PrintTextInGameMenu(gameFieldWidth + 11, 6, "Lives: ", ConsoleColor.DarkGray);
-            PrintTextInGameMenu(gameFieldWidth + 11, 8, "Level: ", ConsoleColor.DarkGray);
+            PrintTextInGameMenu(GameFieldWidth + 4, 0, "--------------------------", ConsoleColor.DarkGray);
+            PrintTextInGameMenu(GameFieldWidth + 6, 1, "*** SCORPICORE RUSH ***", ConsoleColor.DarkGray);
+            PrintTextInGameMenu(GameFieldWidth + 4, 2, "--------------------------", ConsoleColor.DarkGray);
+            PrintTextInGameMenu(GameFieldWidth + 11, 4, "Points: ", ConsoleColor.DarkGray);
+            PrintTextInGameMenu(GameFieldWidth + 11, 6, "Lives: ", ConsoleColor.DarkGray);
+            PrintTextInGameMenu(GameFieldWidth + 11, 8, "Level: ", ConsoleColor.DarkGray);
         }
 
         public static void DrawDownBorder()
         {
-            Console.SetCursorPosition(0, gameFieldHeight + 1);
+            Console.SetCursorPosition(0, GameFieldHeight + 1);
             Console.ForegroundColor = ConsoleColor.DarkGray;
             Console.Write(new String('-', Console.WindowWidth));
             Console.ForegroundColor = ConsoleColor.DarkYellow;
@@ -347,9 +358,9 @@ namespace ScorpicoreRush
 
         public static void GenerateNewRowRocks()
         {
-            for (int j = 0; j < gameFieldWidth; j++)  // First row contains new elements and it is not displayed
+            for (int j = 0; j < GameFieldWidth; j++)  // First row contains new elements and it is not displayed
             {
-                rocks[0, j] = ' ';
+                rocksMatrix[0, j] = ' ';
             }
 
             int randomRocksPerRow = RockRandom.Next(0, maxRocksPerRow); // Random number of the rocks per row between 0 and rocksPerRow (rocksPerRow not included)
@@ -358,20 +369,20 @@ namespace ScorpicoreRush
             {
                 if (Rand.Next(1, 5) == 1) // one of 5 rows have rocks
                 {
-                    int nextPosition = RockRandom.Next(1, gameFieldWidth); // A random position between 1 and WIDTH (inclusive)
-                    int nextRockType = RockRandom.Next(0, rockSymbols.Length); // A random type of rock
-                    rocks[0, nextPosition] = rockSymbols[nextRockType];
+                    int nextPosition = RockRandom.Next(1, GameFieldWidth); // A random position between 1 and WIDTH (inclusive)
+                    int nextRockType = RockRandom.Next(0, RockSymbols.Length); // A random type of rock
+                    rocksMatrix[0, nextPosition] = RockSymbols[nextRockType];
                 }
             }
         }
 
         public static void MoveAllRowsDown()
         {
-            for (int i = gameFieldHeight; i > 0; i--)
+            for (int i = GameFieldHeight; i > 0; i--)
             {
-                for (int j = 0; j < gameFieldWidth; j++)
+                for (int j = 0; j < GameFieldWidth; j++)
                 {
-                    rocks[i, j] = rocks[i - 1, j];  // make current to be == to the previous row
+                    rocksMatrix[i, j] = rocksMatrix[i - 1, j];  // make current to be == to the previous row
                 }
             }
         }
@@ -382,25 +393,25 @@ namespace ScorpicoreRush
             ConsoleColor color = ConsoleColor.White;
 
             // print only the elements with rocks
-            for (int i = 1; i <= gameFieldHeight; i++)
+            for (int i = 1; i <= GameFieldHeight; i++)
             {
-                for (int j = 0; j < gameFieldWidth; j++)
+                for (int j = 0; j < GameFieldWidth; j++)
                 {
 
-                    if (rocks[i, j] == rockSymbols[0])
+                    if (rocksMatrix[i, j] == RockSymbols[0])
                     {
                         color = ConsoleColor.Yellow;
-                        PrintOnPosition(j, i - 1, rocks[i, j].ToString(), color);
+                        PrintOnPosition(j, i - 1, rocksMatrix[i, j].ToString(), color);
                     }
-                    else if (rocks[i, j] == rockSymbols[1])
+                    else if (rocksMatrix[i, j] == RockSymbols[1])
                     {
                         color = ConsoleColor.Red;
-                        PrintOnPosition(j, i - 1, rocks[i, j].ToString(), color);
+                        PrintOnPosition(j, i - 1, rocksMatrix[i, j].ToString(), color);
                     }
-                    else if (rocks[i, j] == rockSymbols[2])
+                    else if (rocksMatrix[i, j] == RockSymbols[2])
                     {
                         color = ConsoleColor.Green;
-                        PrintOnPosition(j, i - 1, rocks[i, j].ToString(), color);
+                        PrintOnPosition(j, i - 1, rocksMatrix[i, j].ToString(), color);
                     }
                 }
             }
